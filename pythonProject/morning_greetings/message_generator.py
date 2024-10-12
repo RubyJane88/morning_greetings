afbb9d8fc08539b0135f0ab1d7e2b8970b421fb6
Ruby Jane <ruby.jane.cabagnot@smartcraft.com>
2024-10-12 22:06:26 +0200

    add message generator module

---
+++ pythonProject/morning_greetings/message_generator.py
@@ -0,0 +1,10 @@
+# message_generatory.py 
+class MessageGenerator:
+    def __init__(self, greeting: str = "Good Morning") -> None:
+        self.greeting = greeting
+def generate_message(self, name: str) -> str:
+    return f"{self.greeting}, {name}! I just want to say that I hope you have a great day!"
+
+
+    
+    
