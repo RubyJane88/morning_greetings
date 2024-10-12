66a1d797519ce186b5b62eeb7b3b09cdec332bb3
Ruby Jane <ruby.jane.cabagnot@smartcraft.com>
2024-10-12 19:26:11 +0200

    add days left to countdown

--- pythonProject/countdown-to-deadline/countdown-to-deadline.py
+++ pythonProject/countdown-to-deadline/countdown-to-deadline.py
@@ -1,12 +1,16 @@
 import datetime
 
-user_greeting = input("enter a greeting for the countdown: ")
-user_goal_deadline = input("enter your goal before the countdown separated by a colon\n")
-print(user_greeting)
-input_goals = user_goal_deadline.split(":")
+user_name_greeting = input("enter your name: ")
+print("Hello " + user_name_greeting + "!" + " Let's start the countdown!")
+user_goals = input("Enter your top 3 goals for the countdown separated by a colon\n")
+my_goals = user_goals.split(":")
+goals = my_goals[0]
 
-greetings = user_greeting[0]
-goals = user_goal_deadline[1]
+user_date = input("Enter your deadline for the countdown in the format dd.mm.yyyy\n")
+datetime.datetime.strptime(user_date, "%d.%m.%Y")
 
-deadline_date = datetime.datetime.strptime(goals, "%d.%m.%Y")
-print(input_goals)
+print("Your top 3 goals are: " + goals)
+print("Your deadline is: " + user_date)
+
+#calculate how many days from now till the deadline 
+print("Days left: " + str((datetime.datetime.strptime(user_date, "%d.%m.%Y") - datetime.datetime.now()).days) + " days left!") 
